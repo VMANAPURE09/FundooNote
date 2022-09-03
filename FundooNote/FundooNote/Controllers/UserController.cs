f63835533d62314940206b9f8b6ea02eed907b77
tutorial6 <manapure.vaibhav542@gmail.com>
2022-09-03 20:45:21 +0530

    Add UC1 Registor User Solution

---
+++ FundooNote/FundooNote/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace FundooNote.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        IUserBL userBL;
+        private IConfiguration _config;
+        public UserController(IUserBL userBL, IConfiguration config)
+        {
+            this.userBL = userBL;
+            this._config = config;
+        }
+        [HttpPost("RegisterUser")]
+        public IActionResult RegisterUser(UserPostModel userPostModel)
+        {
+            try
+            {
+                this.userBL.RegisterUser(userPostModel);
+                return this.Ok(new { sucess = true, status = 200, message = $"Registration sucessful for {userPostModel.Email}" });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
