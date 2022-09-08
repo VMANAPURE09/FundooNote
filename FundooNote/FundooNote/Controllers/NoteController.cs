ae933b1d7d21f80ec5fbf6b3fb7555a77d1cc018
tutorial6 <manapure.vaibhav542@gmail.com>
2022-09-08 07:33:49 +0530

    Add note Sucessfully

---
+++ FundooNote/FundooNote/Controllers/NoteController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RepositoryLayer.Services;
+using System;
+using System.Linq;
+
+namespace FundooNote.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class NoteController : Controller
+    {
+        INoteBL noteBL;
+
+        private IConfiguration _config;
+        private FundooContext fundooContext;
+        public NoteController(INoteBL noteBL, IConfiguration config, FundooContext fundooContext)
+        {
+            this.noteBL = noteBL;
+            this._config = config;
+            this.fundooContext = fundooContext;
+
+        }
+        [Authorize]
+        [HttpPost("AddNote")]
+        public IActionResult AddNote(NoteModel noteModel)
+        {
+            try
+            {
+                //Authorization, match userid from token
+                var Userid = User.Claims.FirstOrDefault(x => x.Type.ToString().Equals("userId", StringComparison.InvariantCultureIgnoreCase));
+                int UserID = Int32.Parse(Userid.Value);
+                this.noteBL.AddNote(UserID, noteModel);
+                return this.Ok(new { success = true, status = 200, message = "Note Added Sucessfully" });
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
